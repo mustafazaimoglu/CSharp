85196e889cf72bc2305d0c46a9e0dc6c4482d15d
mustafazaimoglu <mkuz138@gmail.com>
2021-02-01 11:50:31 +0300

    first commit

---
+++ ReCapProject/DataAccess/Abstract/ICarDal.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface ICarDal
+    {
+        // CRUD
+        void Create(Car car);
+        List<Car> Read();
+        void Update(Car car);
+        void Delete(Car car);
+
+        List<Car> ReadAllByBrandId(int brandId);
+
+
+    }
+}
